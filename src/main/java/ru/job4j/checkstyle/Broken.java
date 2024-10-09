3801b15b46325e3d4dabb504b1ab36e6775f22b7
Denis <dyakimenko@mail.ru>
2024-10-09 23:03:06 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,26 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String VALUE = "";
+    public String surname;
+    public String name;
+    private int sizeOfEmpty = 10;
+
+    Broken() {
+    }
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c) {
+    }
+}
+
+
+
