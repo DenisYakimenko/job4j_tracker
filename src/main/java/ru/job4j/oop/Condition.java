93867a9f8eae6417685a22c38376a4a09db8601a
Denis <dyakimenko@mail.ru>
2024-10-22 20:33:27 +0300

    3. Перегрузить метод max для трех чисел [#122644]

---
+++ src/main/java/ru/job4j/oop/Condition.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+public class Condition {
+
+    public int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    public int max(int a, int b, int c) {
+        return max(max(a, b), c);
+    }
+
+    public int max(int a, int b, int c, int d) {
+        return max(max(a, b), max(c, d));
+    }
+
+    public static void main(String[] args) {
+        Condition finder = new Condition();
+
+        // Примеры использования
+        System.out.println("Max of 2 numbers: " + finder.max(3, 7));
+        System.out.println("Max of 3 numbers: " + finder.max(3, 7, 5));
+        System.out.println("Max of 4 numbers: " + finder.max(3, 7, 5, 9));
+    }
+}
---
+++ src/test/java/ru/job4j/oop/ConditionTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionTest {
+
+    @Test
+    public void elsea3andb7max7() {
+        Condition finder = new Condition();
+        assertEquals(7, finder.max(3, 7));
+
+    }
+
+    @Test
+    public void elsea3andb7andc5max7() {
+        Condition finder = new Condition();
+        assertEquals(7, finder.max(3, 7, 5));
+    }
+
+    @Test
+    public void elsea3andb7andc5andd9max9() {
+        Condition finder = new Condition();
+        assertEquals(9, finder.max(3, 7, 5, 9));
+    }
+}
