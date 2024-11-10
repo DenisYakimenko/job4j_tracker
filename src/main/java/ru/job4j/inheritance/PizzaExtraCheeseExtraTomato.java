fefd2ae181a322a9fe076b48bb5c65d037fd6797
Denis <dyakimenko@mail.ru>
2024-11-10 12:39:11 +0300

    3.2. Удаление моделей из массива. [#310016]

--- src/main/java/ru/job4j/inheritance/PizzaExtraCheeseExtraTomato.java
+++ src/main/java/ru/job4j/inheritance/PizzaExtraCheeseExtraTomato.java
@@ -3,7 +3,6 @@ package ru.job4j.inheritance;
 public class PizzaExtraCheeseExtraTomato extends PizzaExtraCheese {
     @Override
     public String name() {
-        super.name();
         return super.name() + " + " + "extra tomato";
     }
 }
