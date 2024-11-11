ff630062d2a7aa1860306991fd4dcdfbe359f469
Denis <dyakimenko@mail.ru>
2024-11-11 12:16:37 +0300

    5. Tracker - хранилище [#396 #521057]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,15 +14,6 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] result = new Item[size];
-        int count = 0;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item != null) {
-                result[count] = item;
-                count++;
-            }
-        }
         return Arrays.copyOf(items, size);
     }
 
