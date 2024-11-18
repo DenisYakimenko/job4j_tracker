3662b799d14395afa090aa1dfabcbb8d63357cbf
Denis <dyakimenko@mail.ru>
2024-11-18 19:24:50 +0300

    3.1. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -12,7 +12,6 @@ public class Matches {
             String player = turn ? "Первый игрок" : "Второй игрок";
             System.out.println(player + " введите число от 1 до 3:");
             int matches = Integer.parseInt(input.nextLine());
-
             if (matches < 1 || matches > 3) {
                 System.out.println("Ошибка: введите число от 1 до 3.");
             } else if (matches > count) {
