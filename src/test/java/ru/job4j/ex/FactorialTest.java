7623b1e182e2eac245572343b8ad4c18054e7b58
Denis <dyakimenko@mail.ru>
2024-12-08 12:12:27 +0300

    0.3. Тестирование исключений с junit [#219366]

--- src/test/java/ru/job4j/ex/FactorialTest.java
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -3,6 +3,7 @@ package ru.job4j.ex;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FactorialTest {
@@ -15,5 +16,15 @@ class FactorialTest {
                     factorial.calc(-1);
                 });
         assertThat(exception.getMessage()).isEqualTo("Number could not be less than 0");
+    }
+
+    @Test
+    public void whenNumber5Then120(){
+        Factorial factorial = new Factorial();
+        int input = 5;
+        int output = factorial.calc(input);
+        int expected = 120;
+        assertEquals(expected, output,  "Факториал 5 должен быть 120");
+
     }
 }
