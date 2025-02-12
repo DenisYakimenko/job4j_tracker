844e6434dabb5d48f1f2e2e76dcdf6ca411bf8f4
Denis <dyakimenko@mail.ru>
2025-02-12 15:08:13 +0300

    1. Уникальные задачи [#212653 #532332]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -1,5 +1,6 @@
 package ru.job4j.collection;
 
+import java.util.Collections;
 import java.util.HashSet;
 
 public class UniqueText {
@@ -9,7 +10,7 @@ public class UniqueText {
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
         for (String origins : origin) {
-           check.add(origins);
+            check.add(origins);
         }
         for (String texts : text) {
            if (!check.contains(texts)) {
