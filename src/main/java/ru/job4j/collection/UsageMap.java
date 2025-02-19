e770aa5c57228202183267e20c69e787018ad224
Denis <dyakimenko@mail.ru>
2025-02-19 13:12:14 +0300

    0. Map, HashMap [#212652 #532819]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("Denis@mail.ru", "Denis");
+        map.put("semenovich@mail.ru", "Semen Semenovich");
+        map.put("ivan_rabota@yandex.ru", "Ivan Ivanovich");
+        map.put("Denis@mail.ru", "Denis");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
